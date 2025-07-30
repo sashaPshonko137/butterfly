0b9c7ef922f3eca6aa9753ced14e40cdddac6ab0
sashaPshonko137 <privetnitron@gmail.com>
2025-07-30 06:38:48 +0500

    hgjk

---
+++ outfit.js
@@ -0,0 +1,86 @@
+    const outfit = [
+      {
+        type: 'clothing',
+        amount: 1,
+        id: 'body-flesh', // Important
+        account_bound: false,
+        active_palette: 18
+      },
+    {
+        type: 'clothing',
+        amount: 1,
+        id: 'hair_front-n_basic2020overshoulderstraight', // Important
+        account_bound: false,
+        active_palette: 1
+      },//hair_front-n_basic2020overshoulderstraight
+        {
+        type: 'clothing',
+        amount: 1,
+        id: 'hair_back-n_basic2020overshoulderstraight', // Important
+        account_bound: false,
+        active_palette: 1
+      },
+        {
+        type: 'clothing',
+        amount: 1,
+        id: 'hair_back-n_basic2020overshoulderstraight', // Important
+        account_bound: false,
+        active_palette: 1
+      },//eye-n_basic2018femalesquare
+        {
+        type: 'clothing',
+        amount: 1,
+        id: "eye-n_basic2018femaleround", // Important
+        account_bound: false,
+        active_palette: 5
+      },
+              {
+        type: 'clothing',
+        amount: 1,
+        id: "nose-n_basic2018newnose15", // Important
+        account_bound: false,
+        active_palette: 1
+      },//"mouth-basic2018downturnedthinround"
+                    {
+        type: 'clothing',
+        amount: 1,
+        id: "mouth-basic2018downturnedthinround", // Important
+        account_bound: false,
+        active_palette: 52
+      },//"freckle-n_basic2018freckle34"
+                          {
+        type: 'clothing',
+        amount: 1,
+        id: "eyebrow-n_basic2018newbrows15", // Important
+        account_bound: false,
+        active_palette: 1
+      },
+                                {//"shirt-n_starteritems2019tankblack"
+        type: 'clothing',
+        amount: 1,
+        id: "freckle-n_basic2018freckle34", // Important
+        account_bound: false,
+        active_palette: 0
+      },
+                                     {//"pants-n_starteritems2019cuffedjeansblack"
+        type: 'clothing',
+        amount: 1,
+        id: "shirt-n_starteritems2019maletshirtblack", // Important
+        account_bound: false,
+        active_palette: 0
+      },
+                                           {//"pants-n_starteritems2019cuffedjeansblack"
+        type: 'clothing',
+        amount: 1,
+        id: "pants-n_starteritems2019cuffedjeansblack", // Important
+        account_bound: false,
+        active_palette: 0
+      },
+                                                 {//"pants-n_starteritems2019cuffedjeansblack"
+        type: 'clothing',
+        amount: 1,
+        id: "shoes-n_converse_black", // Important
+        account_bound: false,
+        active_palette: 0
+      },
+    ];
